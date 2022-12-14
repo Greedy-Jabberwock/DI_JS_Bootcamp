2130e7cedaf25d5c41187f15cada8ee90dd32e2d
Greedy_Jabberwock <greedy.jabberwock@gmail.com>
2022-12-14 18:53:13 +0200

    Added Week10 Day4 Gold and Ninja

---
+++ Week_10/Day_4/Gold/main.js
@@ -0,0 +1,102 @@
+// Exercise 1: Analyze #2
+
+// let resolveAfter2Seconds = function () {
+//     console.log("starting slow promise");
+//     return new Promise(resolve => {
+//         setTimeout(function () {
+//             resolve("slow");
+//             console.log("slow promise is done");
+//         }, 2000);
+//     });
+// };
+
+// let resolveAfter1Second = function () {
+//     console.log("starting fast promise");
+//     return new Promise(resolve => {
+//         setTimeout(function () {
+//             resolve("fast");
+//             console.log("fast promise is done");
+//         }, 1000);
+//     });
+// };
+
+// let sequentialStart = async function () {
+//     console.log('==SEQUENTIAL START==');
+//     const slow = await resolveAfter2Seconds();
+//     console.log(slow);
+//     const fast = await resolveAfter1Second();
+//     console.log(fast);
+// }
+
+// sequentialStart()
+
+/* Output order:
+==SEQUENTIAL START==
+starting slow promise
+slow promise is done
+slow
+starting fast promise
+fast promise is done
+fast
+*/
+
+// Exercise 2: Analyze #3
+
+// let resolveAfter2Seconds = function () {
+//     console.log("starting slow promise");
+//     return new Promise(resolve => {
+//         setTimeout(function () {
+//             resolve("slow");
+//             console.log("slow promise is done");
+//         }, 2000);
+//     });
+// };
+
+// let resolveAfter1Second = function () {
+//     console.log("starting fast promise");
+//     return new Promise(resolve => {
+//         setTimeout(function () {
+//             resolve("fast");
+//             console.log("fast promise is done");
+//         }, 1000);
+//     });
+// };
+
+// let concurrentStart = async function () {
+//     console.log('==CONCURRENT START with await==');
+//     const slow = resolveAfter2Seconds();
+//     const fast = resolveAfter1Second();
+//     console.log(await slow);
+//     console.log(await fast);
+// }
+
+// setTimeout(concurrentStart, 4000)
+
+/*
+==CONCURRENT START with await==
+starting slow promise
+starting fast promise
+slow
+fast
+fast promise is done
+slow promise is done
+*/
+
+// Exercise 3 : Modify Fetch With Async/Await
+
+const urls = [
+    "https://jsonplaceholder.typicode.com/users",
+    "https://jsonplaceholder.typicode.com/posts",
+    "https://jsonplaceholder.typicode.com/albums"
+  ];
+
+const getData = async function() {
+const [ users, posts, albums ] = await Promise.all(urls.map(async url =>
+  await (await fetch(url)).json()
+));
+console.log('users', users);
+console.log('posta', posts);
+console.log('albums', albums);
+}
+
+getData()
