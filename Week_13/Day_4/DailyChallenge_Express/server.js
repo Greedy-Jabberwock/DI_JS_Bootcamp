6c3afb8aba604bf91d1f7980c881db2546ab3be9
Greedy_Jabberwock <greedy.jabberwock@gmail.com>
2023-01-05 13:31:51 +0200

    Little patch to DailcyChallenge (validation of hobby added)

--- Week_13/Day_4/DailyChallenge_Express/server.js
+++ Week_13/Day_4/DailyChallenge_Express/server.js
@@ -6,6 +6,12 @@ const credentials = {
     HOST: "localhost"
 };
 
+const onlyLetters = (str) => {
+    let str_arr = str.split('')
+    str_arr = str_arr.filter(item => item.match(/[a-zA-Z]/g))
+    return str_arr.length == str.length
+}
+
 app.listen(credentials.PORT, credentials.HOST, () => {
     const server_address = `http://${credentials.HOST}:${credentials.PORT}`
     console.log(`Server is running on ${server_address}`);
@@ -14,7 +20,14 @@ app.listen(credentials.PORT, credentials.HOST, () => {
 app.get('/aboutMe/:hobby', (req, res) => {
     const params = req.params;
     console.log('req.params: ', params)
-    res.send(`<h2>Your hobby is '${params.hobby}'</h2>`);
+    if (onlyLetters(params.hobby)) {
+        res.status(200);
+        res.send(`<h2>Your hobby is '${params.hobby}'</h2>`); 
+    } else {
+        res.status(400);
+        res.send('<h1>400</h1><h3>Bad Request</h3>')
+    }
+    
 });
 
 app.use('/pic', express.static(`${__dirname}/public/picture`));
